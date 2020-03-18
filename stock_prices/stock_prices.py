1659c25569701b83a38c4db498122e578d90ba72
donutwizard666 <thebeardedwarrior@icloud.com>
2020-03-18 16:09:04 -0700

    completed stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,21 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  #setting max profit to -infinity
+  max_profit = float('-inf')
+  # for every item (from 0 to however many items there are)
+  for i in range(0, len(prices)-1):
+    # setting purchase amount to prices list at every index
+    purchase = prices[i]
+    # setting j to the purchase amount to the right of the index
+    for j in range(i+1, len(prices)-1):
+      #setting profit to j subtract i
+      profit = prices[j] - purchase
+      #if the profit is greater than max_profit then set max_profit equal to profit
+      if profit > max_profit:
+        max_profit = profit
+
+  return max_profit 
 
 
 if __name__ == '__main__':
